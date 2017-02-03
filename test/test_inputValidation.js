6a15013c3cae46eb2f0dfd1fe9edccb2169209d3
Brett <brett.sandusky@gmail.com>
2017-02-03 18:01:39 +0000

    add url validation and tests

---
+++ test/test_inputValidation.js
@@ -0,0 +1,43 @@
+'use strict'
+const validate = require("../inputValidation")
+const assert = require("chai").assert
+
+describe("inputValidation", () => {
+  
+  describe("validate()", () => {
+    
+    it("should take in a string", () => {
+      validate("hello, world")
+    })
+    
+    it("should return false if string is not a url", () => {
+      let output = validate("hello, world")
+      assert.isFalse(output)
+    })
+    
+    it("should return true if string is valid url_https", () => {
+      let output = validate("https://www.google.com")
+      assert.isTrue(output)
+    })
+    
+    it("should return true if string is valid url_http", () => {
+      let output = validate("http://www.google.com")
+      assert.isTrue(output)
+    })
+    
+    it("should return true if string is valid url_/search", () => {
+      let output = validate("https://www.google.com/search")
+      assert.isTrue(output)
+    })
+    
+    it("should return true if string is valid url_subdomain", () => {
+      let output = validate("https://search.google.com")
+      assert.isTrue(output)
+    })
+    
+    it("should return true if string is valid url_ip", () => {
+      let output = validate("127.0.0.1:8080")
+      assert.isTrue(output)
+    })
+  })
+})
