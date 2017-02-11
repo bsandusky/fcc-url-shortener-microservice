054e5e7cd1359a2c1fa074dc96ac4ae0874e1a7a
Brett <brett.sandusky@gmail.com>
2017-02-11 18:04:07 +0000

    Store new non-duplicate urls

--- DB.js
+++ DB.js
@@ -1,27 +1,27 @@
 'use strict'
-const mongo = require("mongodb").MongoClient  
-const url = 'mongodb://localhost:27017/test'
+const mongo = require("mongodb").MongoClient
+const url = "mongodb://localhost:27017/test"
 
 module.exports = class DB {
     
     constructor(){}
     
-    insert(urlObj) {
-        mongo.connect(url, (err, db) => {
-            if (err) throw err
-            db.collection('urls').update({original_url: urlObj.original_url}, urlObj, {upsert: true})
-            db.close()
+    getRecordByUrl(input, callback) {
+           mongo.connect(url, (err, db) => {
+            if (err) console.log(err)
+            db.collection('urls').findOne({original_url: input}, (err, result) => {
+                if (err) throw err
+                db.close()
+                callback(result)
+            })
         })
     }
     
-    getShortUrlCode(input, callback) {
+    insert(urlObj) {
         mongo.connect(url, (err, db) => {
             if (err) throw err
-            db.collection('urls').findOne({original_url: input}, (err, doc) => {
-                if (err) throw err
-                callback(doc.short_url_code)
-                db.close()
-            })
+            db.collection('urls').insertOne({original_url: urlObj.original_url, short_url_code: urlObj.short_url_code})
+            db.close()
         })
     }
 }
--- app.js
+++ app.js
@@ -5,19 +5,41 @@ const validate = require("./inputValidation")
 const generateURLCode = require("./generateURLCode")
 const app = express()
 
+app.use((req, res, next) => {
+    
+    if (!process.env.APP_URL) {
+        process.env.APP_URL = "https://fcc-api-projects-bsandusky.c9users.io/"
+    }
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send("Hello, world")
 })
 
-app.get('/new/:input', (req, res) => {
+app.get('/:input', (req, res) => {
+    res.send(req.params.input)
+})
+
+app.get('/new/*', (req, res) => {
     
-    if (!validate(req.params.input)) {
+    if (!validate(req.params[0])) {
         res.send({error: "Not a valid url input"})
+
     } else {
         const db = new DB()
-        let urlObj = {original_url: req.params.input, short_url_code: generateURLCode()}
-        db.insert(urlObj)
-        res.send({success: "Database entry completed", urlInfo: urlObj})
+        
+        db.getRecordByUrl(req.params[0], (result) => {
+            
+            if (result) {
+                res.send({error: "Duplicate record", urlInfo: result})
+        
+            } else {
+                let urlObj = {original_url: req.params[0], short_url_code: generateURLCode()}
+                db.insert(urlObj)
+                res.send({success: "Entry completed", urlInfo: urlObj})
+            }
+        })
     }
 })
 
